059dad04e58b5155d3a1635df1b804fa6f04ce99
joungwan Park <whddhs6645@gmail.com>
2020-05-23 15:51:15 +0000

    11657

--- main.cpp
+++ main.cpp
@@ -1,62 +1,62 @@
 #include <iostream>
-#include <stack>
-#include <string>
 #include <vector>
 using namespace std;
-void nojam1021(){
-  //입력시작
-  int M,N;
-  cin >>M;
-  cin >>N;
-  int* arr=new int[M];
-  for(int i=0;i<M;i++){
-    arr[i]=i+1;
-  }
-  int* target=new int[N];
-  for(int i=0;i<N;i++){
-    cin>>target[i];
-  }
-  //입력끝
-  //함수시작
-  int sum = 0;
-  for(int i=0;i<N;i++){
-    int front=0;
-    for(int j=0;j<M;j++){
-      if(target[i]==arr[j]){
-        break;
-      }else{
-        front++;
-      }
-    }
-    //타겟 넘버까지의 길이 측정완료
-    int middle = M/2;
-    int* cpyarr = new int[M-1];
-    int count = 0;
-    if(front<=middle){
-      //앞으로 밀어주기
-      count = front;
-    }else{
-      //뒤로 밀어주기
-      count = M-front;
-    }
-    //바뀐 배열 정리
-    for(int k=0;k<M-front-1;k++){
-        cpyarr[k]=arr[front+k+1];
-    }
-    int num=0;
-    for(int l=M-front-1;l<M-1;l++){
-      cpyarr[l]=arr[num];
-      num++;
-    }
-    arr= cpyarr;
-    sum+=count;
-    M=M-1;
-  }
-  //함수끝
-  //출력
-  cout << sum << endl;
-}
+
+#define endl "\n"
+#define MAX 501
+#define INF 1<<30
+
+struct edge{
+  int from;
+  int to;
+  int cost;
+};
+
+int N, M;
+long long Dist[MAX];
+vector<edge> Edge;
+
 
 int main() {
-  nojam1021();
+
+  cin >> N >> M;
+  for (int i=1; i<=N;i++) Dist[i] = INF;
+  for (int i=0; i< M; i++){
+    int From, To, Cost;
+    edge tmp;
+    cin >> tmp.from >> tmp.to >> tmp.cost;
+
+    Edge.push_back(tmp);
+  }
+
+  Dist[1] = 0;
+  for (int i=1; i<=N - 1; i++){
+    for(int j=0; j<Edge.size(); j++){
+      int From = Edge[j].from;
+      int To = Edge[j].to;
+      int Cost = Edge[j].cost;
+
+      if (Dist[From] == INF) continue;
+      if (Dist[To] > Dist[From] + Cost) Dist[To] =Dist[From] + Cost;
+    }
+  }
+
+  for (int i = 0 ; i < Edge.size(); i++){
+      int From = Edge[i].from;
+      int To = Edge[i].to;
+      int Cost = Edge[i].cost;
+
+    if (Dist[From] == INF) continue;
+    if (Dist[To] > Dist[From] + Cost){
+      cout << -1 << endl;
+      exit(0);
+    }
+  }
+
+  for (int i=2; i<=N; i++){
+    if (Dist[i] == INF) cout << -1 << endl;
+    else cout << Dist[i]<<endl;
+  }
+
+  return 0;
 }
