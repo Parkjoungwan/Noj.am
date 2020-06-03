02353d6c3ed09d461633e9f8fd89cebc0f78a828
joungwan Park <whddhs6645@gmail.com>
2020-06-03 08:34:44 +0000

    2805

--- main.cpp
+++ main.cpp
@@ -1,54 +1,46 @@
-#include <bits/stdc++.h> 
+#include <iostream>
 using namespace std;
-int dx[4] = {0, 0, 1, -1}; 
-int dy[4] = {1, -1, 0, 0};
-int tomato[1001][1001];
-int d[1001][1001]; 
-int n, m; 
-int main() { 
-  scanf("%d %d", &m, &n);
-  queue<pair<int, int> > q; 
-  for (int i = 0; i < n; i++) { 
-    for (int j = 0; j < m; j++) {
-      scanf("%d", &tomato[i][j]); 
-      d[i][j] = -1; 
-      if (tomato[i][j] == 1) {
-        q.push({i, j}); 
-        d[i][j] = 0; 
-        
-      } 
-      
-    } 
-    
-  } while (!q.empty()) {
-    int x = q.front().first;
-    int y = q.front().second; 
-    q.pop(); 
-    for (int i = 0; i < 4; i++) { 
-      int nx = x + dx[i];
-      int ny = y + dy[i];
-      if (0 <= nx && nx < n && 0 <= ny && ny < m) { 
-        if (tomato[nx][ny] == 0 && d[nx][ny] == -1) {
-          // 익지 않은 토마토 이면서 방문하지 않은 곳
-          d[nx][ny] = d[x][y] + 1;
-          q.push({nx, ny});
-          }
-        } 
-      
+
+void Test(int* arr, int N){
+  for(int i=0;i<N;i++){
+    cout << arr[i];
+  }
+}
+
+void nojam2805(){
+  int N;
+  long long M;
+  cin >> N >> M;
+  long long* Tree=new long long[N];
+  long long High=0;
+  for (int i=0;i<N;i++){
+    cin >>Tree[i];
+    if(Tree[i]>High){
+      High=Tree[i];
     }
-  } 
-  int result = 0;
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      result = max(result,d[i][j]);
-      } 
-  } 
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      if (tomato[i][j] == 0 && d[i][j] == -1) {
-        result = -1; 
-        } 
+  }
+  long long Low = 1;
+  long long result=0;
+  while(Low<=High){
+    long long mid = (High+Low)/2;
+    long long sum=0;
+    for(int i=0;i<N;i++){
+      if(Tree[i]>mid){
+        sum+=Tree[i]-mid;
       }
+    }
+    if(sum>=M){
+      if(result<mid){
+        result=mid;
+      }
+      Low=mid+1;
+    }else{
+      High=mid-1;
+    }
   }
   cout << result;
+}
+
+int main(){
+  nojam2805();
 }
