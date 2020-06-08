967f465333a37a26d8fc40bbdaee1838c94c0a96
joungwan Park <whddhs6645@gmail.com>
2020-06-08 14:56:22 +0000

    2178

--- main.cpp
+++ main.cpp
@@ -1,49 +1,55 @@
 #include <iostream>
-#include <algorithm>
+#include <queue>
+#include <string>
+#include <utility>
+
 using namespace std;
 
-void nojam1920(){
-  int N,M;
-  cin >> N ;
-  int A[N];
-  for( int i=0;i<N;i++){
-    cin >> A[i];
-  }
-  sort(A,A+N);
-  cin >> M ;
-  int B[M];
-  for (int i=0;i<M;i++){
-    cin >> B[i];
-  }
-  int start, end, mid;
-  bool ThereExist;
-  for(int i=0;i<M;i++){
-    start=0;
-    end= N-1;
-    mid= (start+end)/2;
-    ThereExist =false;
-    for (;end-start>=0;){
-      if(A[mid]==B[i]){
-        ThereExist=true;
-        break;
-      }
-      if(A[mid]>B[i]){
-        end=mid-1;
-      }
-      if(A[mid]<B[i]){
-        start=mid+1;
-      }
-      mid= (start+end)/2;
+int N, M;
+int check[100][100] = {
+    0,
+},
+    dx[4] = {0, 1, 0, -1}, dy[4] = {-1, 0, 1, 0};
+bool v[100][100] = {
+    false,
+};
+string arr[100];
+
+void bfs(int i, int j)
+{
+    v[i][j] = true;
+
+    queue<pair<int, int>> q;
+    q.push(make_pair(i, j));
+
+    while (!q.empty())
+    {
+        int x = q.front().second;
+        int y = q.front().first;
+
+        q.pop();
+
+        for (int k = 0; k < 4; k++)
+        {
+            int newX = x + dx[k];
+            int newY = y + dy[k];
+
+            if (0 <= newX && newX < M && 0 <= newY && newY < N && arr[newY][newX] == '1' && !v[newY][newX] && check[newY][newX] == 0)
+            {
+                check[newY][newX] = check[y][x] + 1;
+                v[newY][newX] = true;
+                q.push(make_pair(newY, newX));
+            }
+        }
     }
-    if(ThereExist){
-      cout << "1"<<"\n";
-    }
-    if(!ThereExist){
-      cout << "0"<<"\n";
-    }
-  }
 }
 
-int main(){
-  nojam1920();
+int main()
+{
+    cin >> N >> M;
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
+
+    bfs(0, 0);
+    cout << check[N - 1][M - 1] + 1 << endl;
 }
