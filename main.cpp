a21476b3c071a1f970bf43cc7ade8aeab28b4585
parkjoungwan <park@DESKTOP-2U9FQD8.localdomain>
2020-07-14 16:17:28 +0900

    ubuntu

--- main.cpp
+++ main.cpp
@@ -3,26 +3,27 @@
 #include <algorithm>
 #include <string>
 using namespace std;
- 
-void nojam1475(){
-  string num;
-  cin >> num ;
-  int check[10]={0,};
-  for(int i=0;i<num.size();i++){
-    int N=num[i]-'0';
-    check[N]++;
-  }
-  check[6]=(check[6]+check[9]+1)/2;
-  int max=0;
-  int maxnum=0;
-  for(int i=0;i<9;i++){
-    if(max<check[i]){
-      max=check[i];
-    }
-  }
-  cout << max;
+
+struct Things{
+	int W;
+	int V;
+};
+
+void nojam12865(){
+	int N,K;
+	cin >> N;
+	cin >> K;
+	Things things[N];
+	for(int i=0;i<N;i++){
+		cin>>things[i].W;
+		cin>>things[i].V;
+	}	
+	for(int i=0;i<N;i++){
+		cout<<things[i].W;
+		cout<<things[i].V;
+	}
 }
- 
+
 int main() {
-  nojam1475();
+  nojam12865();
 }
