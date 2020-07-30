ba10e375077b98495b5fcd9a4f2de59a74fe181d
parkjoungwan <park@DESKTOP-2U9FQD8.localdomain>
2020-07-30 10:25:37 +0900

    10989

---
+++ 10989.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int N;
+int Num[10000]={0,};
+void Show(){
+	for(int i=0;i<10000;i++){
+		for(int j=0;j<Num[i];j++){
+			cout << i<<"\n";
+		}
+	}
+}
+
+void nojam10989(){
+	cin >> N;
+	for(int i=0;i<N;i++){
+		int num;
+		cin >>num;
+		Num[num]++;
+	}
+	Show();
+}
+
+int main(){
+	nojam10989();
+}
