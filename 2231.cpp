ba9a6354157d7cff8165be8b1c7c54f55370c0f6
Parkjoungwan <whddhs6645@gmail.com>
2020-08-06 01:06:29 +0900

    2231

--- 2231.cpp
+++ 2231.cpp
@@ -8,6 +8,11 @@ void nojam19532(){
 	cin >> a >> b >> c >> d  >> e >> f ;
 	int up = (a*f) - (d*c);
 	int down = (a*e) - (b*d);
+	if(down==0||a==0){
+		x=1;
+		y=1;	
+		return;
+	}
 	y=up/down;
 	x=(c-(b*y))/a;
 	cout << x <<" "<< y << "\n";
