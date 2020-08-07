070b34d6d9cdfb9efdc6eb50a227f8a72dbc2b93
parkjoungwan <park@DESKTOP-2U9FQD8.localdomain>
2020-08-07 13:52:50 +0900

    1063fix1

--- 1063.cpp
+++ 1063.cpp
@@ -7,8 +7,8 @@ int low,low2;
 int king[2];
 int stone[2];
 
-bool same(){
-	if(row==row2 && low==low2){
+bool same(int rrow,int rrow2){
+	if(rrow==rrow2 && low==low2){
 		return true;
 	}
 	return false;
@@ -22,17 +22,19 @@ void move(string mv){
 		if(rrow<8){
 			//cout << "OUT: R\n";
 			rrow++;
+			//cout << rrow<<endl;
 		}else{
 			return;
 		}
-		if(same()){
+		if(same(rrow,rrow2)){
 			if(rrow2<8){
 				rrow2++;
+				//cout << rrow2 <<endl;
 			}else{
 				rrow--;
+				return;
 			}
 		}
-		return;
 	}
 	else if(mv=="L"){
 		if(rrow>1){
@@ -42,30 +44,32 @@ void move(string mv){
 		}else{
 			return;
 		}
-		if(same()){
+		if(same(rrow,rrow2)){
 			if(rrow2>1){
 				rrow2--;
 			}else{
 				rrow++;
+				return;
 			}
 		}
-		return;
 	}
 	else if(mv=="T"){
 		if(low<8){
 			low++;
-			//cout << "out: T\n";
+			//cout << low<<endl;
 		}else{
 			return;
 		}
-		if(same()){
+		if(same(rrow,rrow2)){
+			//cout << "밀림"<<endl;
 			if(low2<8){
 				low2++;
+				//cout << low2 << endl;
 			}else{
 				low--;
+				return;
 			}
 		}
-		return;
 	}
 	else if(mv=="B"){
 		if(low>1){
@@ -74,14 +78,14 @@ void move(string mv){
 		}else{
 			return;
 		}
-		if(same()){
+		if(same(rrow,rrow2)){
 			if(low2>1){
 				low2--;
 			}else{
 				low++;
+				return;
 			}
 		}
-		return;
 	}
 	else if(mv=="RT"){
 		if(rrow<8&&low<8){
@@ -91,16 +95,16 @@ void move(string mv){
 		}else{
 			return;
 		}
-		if(same()){
+		if(same(rrow,rrow2)){
 			if(low2<8&&rrow2<8){
 				rrow2++;
 				low2++;
 			}else{
 				rrow--;
 				low--;
+				return;
 			}
 		}
-		return;
 	}
 	else if(mv=="RB"){
 		if(rrow<8&&low>1){
@@ -110,16 +114,16 @@ void move(string mv){
 		}else{
 			return;
 		}
-		if(same()){
+		if(same(rrow,rrow2)){
 			if(rrow2<8&&low2>1){
 				rrow2++;
 				low2--;
 			}else{
 				rrow--;
 				low++;
+				return;
 			}
 		}
-		return;
 	}
 	else if(mv=="LT"){
 		if(rrow>1&&low<8){
@@ -129,53 +133,52 @@ void move(string mv){
 		}else{
 			return;
 		}
-		if(same()){
+		if(same(rrow,rrow2)){
 			if(rrow2>1&&low2<8){
 				rrow2--;
 				low2++;
 			}else{
 				rrow++;
 				low--;
+				return;
 			}
 		}
-		return;
 	}
 	else if(mv=="LB"){
 		if(rrow>1&&low>1){
-			row--;
+			rrow--;
 			low--;
 			//cout << "out: LB\n";
 		}else{
 			return;
 		}
-		if(same()){
+		if(same(rrow,rrow2)){
 			if(rrow2>1&&low2>1){
 				rrow2--;
 				low2--;
 			}else{
+				rrow++;
+				low--;
 				return;
 			}
 		}
-		return;
 	}
+	row=(char)(rrow+64);
+	row2=(char)(rrow2+64);
 }
 
 void nojam1063(){
 	//king 위치
 	scanf(" %1c",&row);
 	scanf("%1d",&low);
-	king[0]=(int)row - 64;
-	king[1]=low;
 
 	//stone 위치
 	scanf(" %1c",&row2);
 	scanf("%1d",&low2);
-	stone[0]=(int)row2-64;
-	stone[1]=low2;
 	
 	//cout << "입력끝"<<"\n";
 
-	//명령어 갯수
+	//명령어 갯수 및 위치 수정
 	scanf("%d",&T);
 
 	for(int i=0;i<T;i++){
@@ -183,9 +186,15 @@ void nojam1063(){
 		cin >> command;
 		move(command);
 	}
+	//위치확정
+	stone[0]=(int)row2-64;
+	stone[1]=low2;
+	king[0]=(int)row - 64;
+	king[1]=low;
 	char kingchar = (char)(king[0]+64);
 	char stonechar = (char)(stone[0]+64);
-
+	
+	//결과출력
 	cout << kingchar << king[1] << endl;
 	cout << stonechar << stone[1] << endl;
 }
