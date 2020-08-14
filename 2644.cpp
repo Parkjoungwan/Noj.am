87e94f0a5d1317f1af2459828fc2de5fc09a0967
박종완 <parkjoungwan@bagjong-wan-ui-MacBookPro.local>
2020-08-14 17:47:20 +0900

    2644

---
+++ 2644.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int N;
+int start, end;
+int Ncm;
+queue <pair<int,int>> que;
+int family[101][101];
+void BFS(){
+	q.push(make_pair())
+}
+
+void nojam2644(){
+	cin >> N;
+	cin >> start >> end;
+	cin >> Ncm;
+	for(int i=0;i<Ncm;i++){
+		int x,y;
+		cin >> x >> y;
+		family[x][y]=1;
+		family[y][x]=1;
+	}
+}
+
+int main(){
+
+}
+
