a96babd96560f5f37903eac13894a419080f87ab
박종완 <parkjoungwan@bagjong-wan-ui-MacBookPro.local>
2020-08-17 14:48:55 +0900

    2644 solved

--- 2644.cpp
+++ 2644.cpp
@@ -1,28 +1,45 @@
 #include <iostream>
+#include <vector>
 #include <queue>
+#include <string.h>
+#include <cmath>
 using namespace std;
-int N;
-int start, end;
-int Ncm;
-queue <pair<int,int>> que;
-int family[101][101];
-void BFS(){
-	q.push(make_pair())
+vector<int> arr[101];
+int memo[101];
+int ans = -1;
+void dfs(int p, int c, int cnt){
+    //cout <<"p "<<p<<" c "<< c<<" cnt: "<<cnt<<endl;
+    memo[p] = 1;
+    if(c==p){
+	ans = cnt;
+	return;
+    }
+    int z = cnt;
+    for(int i=0; i<arr[p].size(); i++){
+        if(memo[arr[p][i]]==0){
+            dfs(arr[p][i],c, cnt+1);
+        }
+    }
+    
+    return;
 }
-
-void nojam2644(){
-	cin >> N;
-	cin >> start >> end;
-	cin >> Ncm;
-	for(int i=0;i<Ncm;i++){
-		int x,y;
-		cin >> x >> y;
-		family[x][y]=1;
-		family[y][x]=1;
-	}
-}
-
+ 
 int main(){
-
+    int n, m;
+    int x,y;
+    int a,b;
+    cin>>n>>x>>y>>m;
+    for(int i=0; i<m; i++){
+        cin>> a>>b;
+        arr[a].push_back(b);
+        arr[b].push_back(a);
+    }
+ 
+    dfs(x,y,0);
+    cout <<ans<<endl;
+ 
+ 
 }
+
+
 
---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void  test(){
+	printf("Hello, World\n");
+}
+
+int main(){
+	test();
+	printf("test\n");
+
+}
