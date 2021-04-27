fea2393f6d7ba7023907229105333f9c497d0632
박종완 <whddhs6645@gmail.com>
2021-04-27 20:59:09 +0900

    2098

--- 2098/2098.cpp
+++ 2098/2098.cpp
@@ -3,17 +3,41 @@
 #include <vector>
 #include <cmath>
 #include <cstring>
+#define MAX 16000001
 
 using namespace std;
 
 int n;
 int width[16][16];
 int min(int a, int b){ return a < b ? a : b;}
+int d[1 << 16][16] = {0, };
 
-int ft_dfs(int visit, int )
+int ft_dfs(int visit, int now)
 {
-	int ret;
-	
+	visit |= (1 << now);
+	if (visit == (1 << now) - 1)
+	{
+		if (width[now][0] > 0)
+		{
+			return width[now][0];
+		}
+		return MAX;
+	}
+
+	int& ret = d[visit][now];
+	if (ret > 0)
+		return ret;
+	ret = MAX;
+	for (int i = 0; i < n; i++)
+	{
+		if (i != now && (visit & (1 << i)) == 0 && width[now][i] > 0)
+		{
+			int temp = ft_dfs(visit, i) + width[now][i];
+			if (ret > temp)
+				ret = temp;
+		}
+	}
+	return ret;	
 }
 
 int main()
@@ -30,6 +54,9 @@ int main()
 			cin >> width[i][j];
 		}
 	}
-	cout << ft_dfs(0);
+	int ans = ft_dfs(0, 0);
+	if (ans == MAX) cout << "-1";
+	else cout << ans;
 
+	return 0;
 }
